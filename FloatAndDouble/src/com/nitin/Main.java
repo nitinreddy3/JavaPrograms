d39dd02f753ee0d79ce8fb9198375edc8f256db6
nitin reddy <reddynitin3@gmail.com>
2018-02-20 16:53:56 +0530

    Examples on float and double typed concept

---
+++ FloatAndDouble/src/com/nitin/Main.java
@@ -0,0 +1,15 @@
+package com.nitin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myIntValue = 5 / 2;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+        
+    }
+}
