e898ebae84ad5458c943edf1c9ab56fb2a4e9593
nitin reddy <reddynitin3@gmail.com>
2018-02-22 17:40:33 +0530

    Example on char and boolean

---
+++ CharAndBoolean/src/com/nitin/Main.java
@@ -0,0 +1,17 @@
+package com.nitin;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        char registeredChar = '\u00AE';
+
+        System.out.println("Unicode for registered symbol is "  + registeredChar);
+    }
+}
